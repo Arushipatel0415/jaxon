6bb31792d602e4fd65ccb398248b1d90d3707a01
Arushipatel0415 <95451573+Arushipatel0415@users.noreply.github.com>
2021-12-31 18:43:17 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+function preload(){
+  //pre-load images
+  
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+}
+
+function draw() {
+  background(0);
+if(Path.y > 400 ){
+}
+Path=createSprite(200,200);
+path.addImage(pathimg);
+path.velocityY=4;
+path.scale=1.2;
+}
